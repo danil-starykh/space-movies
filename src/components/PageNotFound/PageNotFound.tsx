5736dba6d6a4dd463aa63251e8f8a29a5fdb5cbd
oldman-star <staryh.danil@gmail.com>
2022-10-11 17:55:48 +0300

    created file styles for the not found page

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -1,32 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-
-const classes = {
-  notFoundWrapper: { 
-    pt: '60px', 
-    display: 'flex', 
-    flexDirection: 'column', 
-    justifyContent: 'center', 
-    alignItems: 'center', 
-    width: '100%' 
-  },
-  notFoundIcon: { 
-    color: 'primary.light',
-    width: '10%', 
-    height: 'auto',
-    minWidth: '65px'
-  },
-  notFoundErrorCode: { 
-    color: 'primary.light',
-    fontSize: {xs: '2rem', sm: '2.5rem', md: '3rem' }
-  },
-  notFoundText: { 
-    pl: '10px',
-    color: 'primary.light',
-    fontSize: {xs: '1rem', sm: '1.2rem', md: '1.6rem' }
-  }
-}
+import { classes } from './styles';
 
 const PageNotFound: React.FC = () => {
   return (
---
+++ src/components/PageNotFound/styles.ts
@@ -0,0 +1,25 @@
+export const classes = {
+  notFoundWrapper: { 
+    pt: '60px', 
+    display: 'flex', 
+    flexDirection: 'column', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    width: '100%' 
+  },
+  notFoundIcon: { 
+    color: 'primary.light',
+    width: '10%', 
+    height: 'auto',
+    minWidth: '65px'
+  },
+  notFoundErrorCode: { 
+    color: 'primary.light',
+    fontSize: {xs: '2rem', sm: '2.5rem', md: '3rem' }
+  },
+  notFoundText: { 
+    pl: '10px',
+    color: 'primary.light',
+    fontSize: {xs: '1rem', sm: '1.2rem', md: '1.6rem' }
+  }
+};
