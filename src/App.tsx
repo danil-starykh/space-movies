4aba4b7f0bf48fe80b2ea8cb1f17ea406757f360
oldman-star <staryh.danil@gmail.com>
2022-10-20 01:50:39 +0300

    small refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import MainLayout from './components/MainLayout/MainLayout';
 import MainContent from './components/MainContent/MainContent';
+import { setupStore } from './store';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './styles/Styles';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+const store = setupStore();
 
 const App: React.FC = () => {
   return (
-    <MainLayout>
-      <MainContent/>
-    </MainLayout>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <MainLayout>
+            <MainContent/>
+          </MainLayout>
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,10 @@
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
 import './styles/index.scss';
-import { setupStore } from './store';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './styles/Styles';
-import { BrowserRouter } from 'react-router-dom';
 
-const store = setupStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </Provider>
-);
 
+root.render(<App />);
